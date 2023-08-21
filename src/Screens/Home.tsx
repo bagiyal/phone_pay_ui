72b46bdfd757f873a1929ca2ff9b903d4d0babe1
Abhishek Bagiyal <63339447+bagiyal@users.noreply.github.com>
2023-08-21 23:49:32 +0530

    feature: adder banner

--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -15,11 +15,27 @@ const Home = () => {
             <View style={{ marginLeft: moderateScale(10) }}>
               <View style={{ flexDirection: 'row', alignItems: 'center', }}>
                 <Text style={styles.home}>Home</Text>
-                <Image source={require('../../assets/arrow-down-sign-to-navigate.png')} style={styles.downArrow}/>
+                <Image source={require('../../assets/arrow-down-sign-to-navigate.png')} style={styles.downArrow} />
               </View>
-              <Text>Post Office Chidderwala, Rishikesh </Text>
+              <Text style={styles.address}>Post Office Chidderwala, Rishikesh </Text>
             </View>
           </View>
+          <View style={styles.headerRight}>
+              <Image source={require('../../assets/qr-code.png')} style={styles.rightLogo} />
+              <Image source={require('../../assets/question.png')} style={styles.rightLogo} />
+              <Image source={require('../../assets/ringing.png')} style={styles.rightLogo} />
+          </View>
+        </View>
+      </View>
+      <View style={styles.banner}>
+        <View style={styles.bannerInside}>
+        <Image source={require('../../assets/png-phone-pe-transparent-png.png')} style={styles.bannerLogo} />
+        <Text style={styles.banner_header}> App Update Available </Text>
+        </View>
+        <Text style={styles.header_para}>We need fixed some issues  and addded some cool features in this update </Text>
+        <View style={{right:0,}}>
+          <Text>Later</Text>
+          <Text Update ></Text>
         </View>
       </View>
     </View>
@@ -31,7 +47,7 @@ export default Home
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: '#f2f2f2',
   },
   header: {
     width: '100%',
@@ -73,4 +89,52 @@ const styles = StyleSheet.create({
     marginLeft: moderateScale(5),
     tintColor: 'white',
   },
+  address: {
+    color: 'white',
+    fontSize: moderateScale(12),
+    marginTop: scale(2),
+  },
+  headerRight: {
+    // flex:1,
+    flexDirection: 'row',
+    alignContent: 'center',
+    marginLeft: moderateScale(8),
+    marginTop: moderateScale(-2), 
+  },
+  rightLogo: {
+    width: scale(25),
+    height: scale(25),
+    tintColor:'white',
+    margin: moderateScale(4),
+  },
+  banner:{
+    width: '92%',
+    height: scale(90),
+    backgroundColor:'white',
+    marginTop: '4%',
+    marginLeft:'4%',
+  },
+  banner_header: {
+    fontSize: scale(17),
+    marginTop: scale(5),
+    color:'black',
+    // fontStyle:'italic',
+    fontWeight:'700',
+  },
+  bannerLogo: {
+    width: scale(35),
+    height: scale(35),
+  },
+  bannerInside:{
+    flex:1,
+    flexDirection:'row',
+    marginTop: scale(5),
+    marginLeft:scale(8),
+  },
+  header_para:{
+    marginLeft: scale(15),
+    marginTop: scale(40),
+    position:'absolute',
+  }
+
 })
