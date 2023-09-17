2a13bb6056510f907115dfa1ff9188d3b48aa933
Abhishek Bagiyal <63339447+bagiyal@users.noreply.github.com>
2023-09-17 22:31:37 +0530

    complete password page

--- src/Screens/PayNow.tsx
+++ src/Screens/PayNow.tsx
@@ -1,164 +1,295 @@
-import { View, Text, StyleSheet, FlatList, TouchableOpacity, TextInput, Button, Image } from 'react-native'
-import React, { useState } from 'react'
-import { moderateScale, moderateVerticalScale, scale, verticalScale } from 'react-native-size-matters';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  TextInput,
+  Button,
+  Image,
+} from 'react-native';
+import React, {useState} from 'react';
+import {
+  moderateScale,
+  moderateVerticalScale,
+  scale,
+  verticalScale,
+} from 'react-native-size-matters';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import Entypo from 'react-native-vector-icons/Entypo'
-import { useNavigation } from '@react-navigation/native';
+import Entypo from 'react-native-vector-icons/Entypo';
+import {useNavigation} from '@react-navigation/native';
 import Modal from 'react-native-modal';
 const PayNow = () => {
-    const navigation = useNavigation();
-    const handleBack = () => {
-        console.log(navigation.canGoBack())
-        if (navigation.canGoBack()) {
-            navigation.goBack();
-        }
+  const navigation = useNavigation();
+  const handleBack = () => {
+    console.log(navigation.canGoBack());
+    if (navigation.canGoBack()) {
+      navigation.goBack();
     }
-    const [modealOpen,setModalOpen] = useState(false);
-    const [money,setMoney] = useState(null);
-    return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <View style={styles.subHeader}>
-                    <TouchableOpacity onPress={handleBack}>
-                        <Icon name="arrow-back" size={30} color="white" />
-                    </TouchableOpacity>
-                    <Text style={{ color: 'white', fontSize: 17 }}>Pay Now</Text>
-                    <Icon name='question-mark-circle-outline' size={30} color="white" />
-                </View>
-            </View>
-            <View style={styles.card}>
-                <View style={styles.subCard}>
-                    <View style={styles.nameCircle}>
-                        <Text style={{ textAlign: 'center', marginTop: moderateScale(5), fontSize: 25 }}>AB</Text>
-                    </View>
-                    <View style={{ marginTop: 10, marginLeft: 5 }}>
-                        <Text style={{ fontSize: 22, color: 'black' }}>Aayu</Text>
-                        <Text>ineedhelp@ybl</Text>
-                    </View>
-                    <Text style={{ position: 'absolute', right: 10, marginTop: moderateScale(20), fontSize: 18, color: 'purple' }}>View History</Text>
-
-                </View>
-                <View>
-                    <Icon name='currency-rupee' size={30} color="black" style={{ left: 20, top: 20 }} />
-                    <TextInput
-                        placeholderTextColor={'#929292'}
-                        placeholder='Enter Amount'
-                        // value={money}
-                        onChangeText={txt => setMoney(txt)}
-                        keyboardType='number-pad'
-                        style={styles.input}
-                    />
-                    <TextInput
-                        placeholderTextColor={'#929292'}
-                        placeholder='Add a message (Optional)'
-                        style={styles.input2}
-                    />
-                </View>
-
-            </View>
-            <View style={{ position: 'absolute', bottom: 0, backgroundColor: 'purple', width: '100%', height: verticalScale(60) }}>
-                <TouchableOpacity onPress={() => {setModalOpen(true)}}>
-                    <Text style={{ fontSize: 22, left: moderateScale(150), color: 'white', marginTop: 15 }}>Pay Now</Text>
-                </TouchableOpacity>
-            </View>
-            <Modal isVisible={modealOpen} backdropOpacity={.2} style={styles.modalView}>
-                <View style={styles.mainView}>
-                    <View style={{ height: verticalScale(60), borderBottomWidth: 1, borderBottomColor: 'black', margin: 10, flexDirection: 'row' }}>
-                        <Text style={{ fontSize: 17, color: 'black', margin: 6, fontWeight: '600' }}>Total Payable </Text>
-                        <Text style={{ fontSize: 22, color: 'black', margin: 4, marginLeft: 110, }}>₹ {money} </Text>
-                        <TouchableOpacity onPress={()=>{setModalOpen(false)}} style={{ position: 'absolute', right: 5, }} >
-                        <Icon name='close' size={35} color="black"/>
-                        </TouchableOpacity>
-                    </View>
-                    <View style={styles.bankView}>
-                        <Image source={require('../../assets/hdfc.png')} style={{ height: 40, width: 40, margin: 8, }} />
-                        <Text style={{ fontStyle: 'italic', color: 'black', top: 20, marginLeft: 20, fontSize: 16 }}>HDFC Bank *****55 </Text>
-                        <Image source={require('../../assets/upi.jpg')} style={{ height: 14, width: 60, margin: 8, marginLeft: 30, marginTop: 23 }} />
-                        <Image source={require('../../assets/right_tick.jpg')} style={{ height: 30, width: 30, margin: 8, marginLeft: 30, marginTop: 14, }} />
-                        <View style={{ position: 'absolute', backgroundColor: 'purple', width: '85%', height: verticalScale(50),marginLeft:30,marginTop:moderateScale(80),borderRadius:moderateScale(30) }}>
-                <TouchableOpacity onPress={() => {navigation.navigate('UpiPass')}}>
-                    <Text style={{ fontSize: 20, left: moderateScale(100), color: 'white', marginTop: 12 }}>Pay Now ₹ {money} </Text>
-                </TouchableOpacity>
-            </View>
-                    </View>
-                </View>
-            </Modal>
+  };
+  const [modealOpen, setModalOpen] = useState(false);
+  const [money, setMoney] = useState(null);
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.subHeader}>
+          <TouchableOpacity onPress={handleBack}>
+            <Icon name="arrow-back" size={30} color="white" />
+          </TouchableOpacity>
+          <Text style={{color: 'white', fontSize: 17}}>Pay Now</Text>
+          <Icon name="question-mark-circle-outline" size={30} color="white" />
         </View>
-    )
-}
+      </View>
+      <View style={styles.card}>
+        <View style={styles.subCard}>
+          <View style={styles.nameCircle}>
+            <Text
+              style={{
+                textAlign: 'center',
+                marginTop: moderateScale(5),
+                fontSize: 25,
+              }}>
+              AB
+            </Text>
+          </View>
+          <View style={{marginTop: 10, marginLeft: 5}}>
+            <Text style={{fontSize: 22, color: 'black'}}>Aayu</Text>
+            <Text>ineedhelp@ybl</Text>
+          </View>
+          <Text
+            style={{
+              position: 'absolute',
+              right: 10,
+              marginTop: moderateScale(20),
+              fontSize: 18,
+              color: 'purple',
+            }}>
+            View History
+          </Text>
+        </View>
+        <View>
+          <Icon
+            name="currency-rupee"
+            size={30}
+            color="black"
+            style={{left: 20, top: 20}}
+          />
+          <TextInput
+            placeholderTextColor={'#929292'}
+            placeholder="Enter Amount"
+            // value={money}
+            onChangeText={txt => setMoney(txt)}
+            keyboardType="number-pad"
+            style={styles.input}
+          />
+          <TextInput
+            placeholderTextColor={'#929292'}
+            placeholder="Add a message (Optional)"
+            style={styles.input2}
+          />
+        </View>
+      </View>
+      <View
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          backgroundColor: 'purple',
+          width: '100%',
+          height: verticalScale(60),
+        }}>
+        <TouchableOpacity
+          onPress={() => {
+            setModalOpen(true);
+          }}>
+          <Text
+            style={{
+              fontSize: 22,
+              left: moderateScale(150),
+              color: 'white',
+              marginTop: 15,
+            }}>
+            Pay Now
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <Modal
+        isVisible={modealOpen}
+        backdropOpacity={0.2}
+        style={styles.modalView}>
+        <View style={styles.mainView}>
+          <View
+            style={{
+              height: verticalScale(60),
+              borderBottomWidth: 1,
+              borderBottomColor: 'black',
+              margin: 10,
+              flexDirection: 'row',
+            }}>
+            <Text
+              style={{
+                fontSize: 17,
+                color: 'black',
+                margin: 6,
+                fontWeight: '600',
+              }}>
+              Total Payable{' '}
+            </Text>
+            <Text
+              style={{
+                fontSize: 22,
+                color: 'black',
+                margin: 4,
+                marginLeft: 110,
+              }}>
+              ₹ {money}{' '}
+            </Text>
+            <TouchableOpacity
+              onPress={() => {
+                setModalOpen(false);
+              }}
+              style={{position: 'absolute', right: 5}}>
+              <Icon name="close" size={35} color="black" />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.bankView}>
+            <Image
+              source={require('../../assets/hdfc.png')}
+              style={{height: 40, width: 40, margin: 8}}
+            />
+            <Text
+              style={{
+                fontStyle: 'italic',
+                color: 'black',
+                top: 20,
+                marginLeft: 20,
+                fontSize: 16,
+              }}>
+              HDFC Bank *****55{' '}
+            </Text>
+            <Image
+              source={require('../../assets/upi.jpg')}
+              style={{
+                height: 14,
+                width: 60,
+                margin: 8,
+                marginLeft: 30,
+                marginTop: 23,
+              }}
+            />
+            <Image
+              source={require('../../assets/right_tick.jpg')}
+              style={{
+                height: 30,
+                width: 30,
+                margin: 8,
+                marginLeft: 30,
+                marginTop: 14,
+              }}
+            />
+            <View
+              style={{
+                position: 'absolute',
+                backgroundColor: 'purple',
+                width: '85%',
+                height: verticalScale(50),
+                marginLeft: 30,
+                marginTop: moderateScale(80),
+                borderRadius: moderateScale(30),
+              }}>
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate('UpiPass');
+                }}>
+                <Text
+                  style={{
+                    fontSize: 20,
+                    left: moderateScale(100),
+                    color: 'white',
+                    marginTop: 12,
+                  }}>
+                  Pay Now ₹ {money}{' '}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#f2f2f2',
-    },
-    header: {
-        width: '100%',
-        height: verticalScale(60),
-        backgroundColor: 'purple',
-    },
-    subHeader: {
-        marginTop: '5%',
-        margin: '5%',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    card: {
-        backgroundColor: 'white',
-        height: verticalScale(220),
-    },
-    subCard: {
-        flexDirection: 'row',
-    },
-    nameCircle: {
-        width: moderateScale(50),
-        height: moderateScale(50),
-        borderRadius: scale(50),
-        backgroundColor: 'grey',
-        margin: '3%',
-    },
-    input: {
-        width: '80%',
-        // height: moderateScale(50),
-        // margin:30,
-        borderColor: 'purple',
-        borderWidth: moderateVerticalScale(1),
-        marginLeft: '15%',
-        marginTop: moderateScale(-20),
-        fontSize: 18,
-        fontWeight: 'bold',
-    },
-    input2: {
-        width: '80%',
-        // height: moderateScale(50),
-        // margin:30,
-        borderColor: 'purple',
-        borderWidth: moderateVerticalScale(1),
-        marginLeft: '15%',
-        marginTop: moderateScale(20),
-        fontSize: 18,
-        fontWeight: 'bold',
-    },
-    modalView: {
-        margin: 0,
-    },
-    mainView: {
-        backgroundColor: 'white',
-        width: '100%',
-        height: verticalScale(250),
-        position: 'absolute',
-        bottom: 0,
-        borderTopLeftRadius: moderateScale(20),
-        borderTopRightRadius: moderateScale(20),
-    },
-    bankView: {
-        marginTop:moderateScale(20),
-        height: verticalScale(50),
-        width: '100%',
-        backgroundColor: 'grey',
-        flexDirection: 'row',
-        textAlign: 'center'
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#f2f2f2',
+  },
+  header: {
+    width: '100%',
+    height: verticalScale(60),
+    backgroundColor: 'purple',
+  },
+  subHeader: {
+    marginTop: '5%',
+    margin: '5%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  card: {
+    backgroundColor: 'white',
+    height: verticalScale(220),
+  },
+  subCard: {
+    flexDirection: 'row',
+  },
+  nameCircle: {
+    width: moderateScale(50),
+    height: moderateScale(50),
+    borderRadius: scale(50),
+    backgroundColor: 'grey',
+    margin: '3%',
+  },
+  input: {
+    width: '80%',
+    // height: moderateScale(50),
+    // margin:30,
+    borderColor: 'purple',
+    borderWidth: moderateVerticalScale(1),
+    marginLeft: '15%',
+    marginTop: moderateScale(-20),
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  input2: {
+    width: '80%',
+    // height: moderateScale(50),
+    // margin:30,
+    borderColor: 'purple',
+    borderWidth: moderateVerticalScale(1),
+    marginLeft: '15%',
+    marginTop: moderateScale(20),
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  modalView: {
+    margin: 0,
+  },
+  mainView: {
+    backgroundColor: 'white',
+    width: '100%',
+    height: verticalScale(250),
+    position: 'absolute',
+    bottom: 0,
+    borderTopLeftRadius: moderateScale(20),
+    borderTopRightRadius: moderateScale(20),
+  },
+  bankView: {
+    marginTop: moderateScale(20),
+    height: verticalScale(50),
+    width: '100%',
+    backgroundColor: 'grey',
+    flexDirection: 'row',
+    textAlign: 'center',
+  },
 });
 
-export default PayNow
+export default PayNow;
--- src/Screens/UpiPass.tsx
+++ src/Screens/UpiPass.tsx
@@ -1,85 +1,282 @@
-import { StyleSheet, Text, View,TouchableOpacity } from 'react-native'
-import React from 'react'
-import { moderateScale, verticalScale } from 'react-native-size-matters'
+import {StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {moderateScale, scale, verticalScale} from 'react-native-size-matters';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const UpiPass = () => {
+  const [pin, setPin] = useState('');
+  useEffect(() => {
+    console.log('UpiPass',pin);
+  },[pin])
+  const handleCancel = () => {
+    const pinLenght:number = pin.length;
+    if (pinLenght>0) {
+      setPin(pin.substring(0, pinLenght - 1));
+    }
+    console.log('ss--',pin);
+
+  }
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-      <Text style={{fontSize:20,color:'black',opacity:.9,marginTop:8}}>Cancel</Text>
+        <Text
+          style={{fontSize: 20, color: 'black', opacity: 0.9, marginTop: 8}}>
+          Cancel
+        </Text>
+        <View style={styles.bankView}>
+          <Image
+            source={require('../../assets/hdfc.png')}
+            style={{height: 40, width: 40, margin: 8}}
+          />
+          <Text
+            style={{
+              fontStyle: 'italic',
+              color: 'black',
+              top: 20,
+              marginLeft: 20,
+              fontSize: 16,
+            }}>
+            HDFC Bank *****55{' '}
+          </Text>
+          <Image
+            source={require('../../assets/upi.jpg')}
+            style={{
+              height: 14,
+              width: 60,
+              margin: 8,
+              marginLeft: 90,
+              marginTop: 23,
+            }}
+          />
+        </View>
+        <View style={styles.sender_recvier}>
+          <Text
+            style={{
+              fontStyle: 'italic',
+              color: 'black',
+              top: 10,
+              marginLeft: 20,
+              fontSize: 16,
+            }}>
+            To
+          </Text>
+          <Text
+            style={{
+              fontStyle: 'italic',
+              color: 'black',
+              top: 10,
+              marginLeft: 20,
+              fontSize: 16,
+            }}>
+            Sending
+          </Text>
+
+          <Text
+            style={{
+              fontStyle: 'italic',
+              color: 'black',
+              top: 10,
+              right: 20,
+              fontSize: 16,
+              position: 'absolute',
+            }}>
+            ineedhelp@ybl
+          </Text>
+          <Text
+            style={{
+              fontStyle: 'italic',
+              color: 'black',
+              top: 30,
+              right: 20,
+              fontSize: 16,
+              position: 'absolute',
+            }}>
+            {'₹ 21'}
+          </Text>
+        </View>
+      </View>
+      <View style={styles.pinCode}>
+        <Text style={{fontSize: 19, color: 'black', fontWeight: '600'}}>
+          Enter 4-Digit Upi Pin
+        </Text>
+        <View style={{justifyContent: 'center', flexDirection: 'row'}}>
+          <View style={[styles.pinView,{backgroundColor:(pin.length == 1 || pin.length == 2 || pin.length == 3 || pin.length == 4)?'black':'white'}]}></View>
+          <View style={[styles.pinView,{backgroundColor: (pin.length == 2 || pin.length == 3 || pin.length == 4)?'black':'white'}]}></View>
+          <View style={[styles.pinView,{backgroundColor: (pin.length == 3 || pin.length == 4)?'black':'white'}]}></View>
+          <View style={[styles.pinView,{backgroundColor: (pin.length == 4)?'black':'white'}]}></View>
+        </View>
       </View>
       <View style={styles.numpad}>
-        <View style={{flexDirection:'row',width:'100%'}}>
-          <TouchableOpacity style={styles.numberStyle}>
-            <Text style={{color:'black',fontSize: moderateScale(25)}}>1</Text>
+        <View style={{flexDirection: 'row', width: '100%'}}>
+          <TouchableOpacity
+            activeOpacity={1}
+            style={styles.numberStyle}
+            onPress={() => {
+              if (pin.length < 4) {
+                setPin(pin + '1');
+              }
+            }}>
+            <Text style={{color: 'black', fontSize: moderateScale(25)}}>1</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.numberStyle}>
-            <Text style={{color:'black',fontSize: moderateScale(25)}}>2</Text>
+          <TouchableOpacity
+            activeOpacity={1}
+            style={styles.numberStyle}
+            onPress={() => {
+              if (pin.length < 4) {
+                setPin(pin + '2');
+              }
+            }}>
+            <Text style={{color: 'black', fontSize: moderateScale(25)}}>2</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.numberStyle}>
-            <Text style={{color:'black',fontSize: moderateScale(25)}}>3</Text>
+          <TouchableOpacity
+            activeOpacity={1}
+            style={styles.numberStyle}
+            onPress={() => {
+              if (pin.length < 4) {
+                setPin(pin + '3');
+              }
+            }}>
+            <Text style={{color: 'black', fontSize: moderateScale(25)}}>3</Text>
           </TouchableOpacity>
         </View>
-        <View style={{flexDirection:'row',width:'100%'}}>
-          <TouchableOpacity style={styles.numberStyle}>
-            <Text style={{color:'black',fontSize: moderateScale(25)}}>4</Text>
+        <View style={{flexDirection: 'row', width: '100%'}}>
+          <TouchableOpacity
+            onPress={() => {
+              if (pin.length < 4) {
+                setPin(pin + '4');
+              }
+            }}
+            activeOpacity={1}
+            style={styles.numberStyle}>
+            <Text style={{color: 'black', fontSize: moderateScale(25)}}>4</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.numberStyle}>
-            <Text style={{color:'black',fontSize: moderateScale(25)}}>5</Text>
+          <TouchableOpacity
+            activeOpacity={1}
+            style={styles.numberStyle}
+            onPress={() => {
+              if (pin.length < 4) {
+                setPin(pin + '5');
+              }
+            }}>
+            <Text style={{color: 'black', fontSize: moderateScale(25)}}>5</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.numberStyle}>
-            <Text style={{color:'black',fontSize: moderateScale(25)}}>6</Text>
+          <TouchableOpacity
+            activeOpacity={1}
+            style={styles.numberStyle}
+            onPress={() => {
+              if (pin.length < 4) {
+                setPin(pin + '6');
+              }
+            }}>
+            <Text style={{color: 'black', fontSize: moderateScale(25)}}>6</Text>
           </TouchableOpacity>
         </View>
-        <View style={{flexDirection:'row',width:'100%'}}>
-          <TouchableOpacity style={styles.numberStyle}>
-            <Text style={{color:'black',fontSize: moderateScale(25)}}>7</Text>
+        <View style={{flexDirection: 'row', width: '100%'}}>
+          <TouchableOpacity
+            activeOpacity={1}
+            style={styles.numberStyle}
+            onPress={() => {
+              if (pin.length < 4) {
+                setPin(pin + '7');
+              }
+            }}>
+            <Text style={{color: 'black', fontSize: moderateScale(25)}}>7</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.numberStyle}>
-            <Text style={{color:'black',fontSize: moderateScale(25)}}>8</Text>
+          <TouchableOpacity
+            activeOpacity={1}
+            style={styles.numberStyle}
+            onPress={() => {
+              if (pin.length < 4) {
+                setPin(pin + '8');
+              }
+            }}>
+            <Text style={{color: 'black', fontSize: moderateScale(25)}}>8</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.numberStyle}>
-            <Text style={{color:'black',fontSize: moderateScale(25)}}>9</Text>
+          <TouchableOpacity
+            activeOpacity={1}
+            style={styles.numberStyle}
+            onPress={() => {
+              if (pin.length < 4) {
+                setPin(pin + '9');
+              }
+            }}>
+            <Text style={{color: 'black', fontSize: moderateScale(25)}}>9</Text>
           </TouchableOpacity>
         </View>
-        <View style={{flexDirection:'row',width:'100%'}}>
-          <TouchableOpacity style={styles.numberStyle}>
-          <Icon name='close' size={25} color="black"/>
+        <View style={{flexDirection: 'row', width: '100%'}}>
+          <TouchableOpacity style={styles.numberStyle} activeOpacity={1} onPress={handleCancel}>
+            <Icon name="close" size={25} color="black" />
           </TouchableOpacity>
-          <TouchableOpacity style={styles.numberStyle}>
-            <Text style={{color:'black',fontSize: moderateScale(25)}}>0</Text>
+          <TouchableOpacity
+            activeOpacity={1}
+            style={styles.numberStyle}
+            onPress={() => {
+              if (pin.length < 4) {
+                setPin(pin + '0');
+              }
+            }}>
+            <Text style={{color: 'black', fontSize: moderateScale(25)}}>0</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.numberStyle}>
-            <Text style={{color:'black',fontSize: moderateScale(16)}}>Submit</Text>
+          <TouchableOpacity activeOpacity={1} style={styles.numberStyle}>
+            <Text style={{color: 'black', fontSize: moderateScale(16)}}>
+              Submit
+            </Text>
           </TouchableOpacity>
         </View>
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default UpiPass
+export default UpiPass;
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
   },
-  header:{
+  header: {
     margin: 15,
     height: '8%',
     width: '100%',
-    borderBottomWidth:.5,
+    borderBottomWidth: 0.5,
   },
   numpad: {
-    position:'absolute',
-    bottom:0,
+    position: 'absolute',
+    bottom: 0,
   },
-  numberStyle:{
+  numberStyle: {
     width: '33%',
     height: verticalScale(45),
     borderWidth: 0.4,
     justifyContent: 'center',
     alignItems: 'center',
-  }
-})
+  },
+  bankView: {
+    marginTop: moderateScale(50),
+    height: verticalScale(50),
+    width: '100%',
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    textAlign: 'center',
+  },
+  sender_recvier: {
+    marginTop: moderateScale(20),
+    height: verticalScale(50),
+    width: '100%',
+    backgroundColor: 'grey',
+    flexDirection: 'column',
+    textAlign: 'center',
+  },
+  pinCode: {
+    alignItems: 'center',
+    marginTop: scale(160),
+  },
+  pinView: {
+    width: scale(16),
+    height: scale(16),
+    // backgroundColor: 'black',
+    borderRadius: scale(50),
+    borderWidth: 0.8,
+    margin: scale(7),
+  },
+});
